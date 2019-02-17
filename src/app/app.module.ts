33f3d3dd61c09c8d4bd8b9fcd50de78415b8e3a8
dhruvsharma <dhruv.sharma@accoliteindia.com>
2019-02-17 19:24:32 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {SuiModule} from 'ng2-semantic-ui';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
